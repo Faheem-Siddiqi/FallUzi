0bc6ccfcc35e0787a1c383b1f96e4242c321d3d1
Faheem-Siddiqi <faheem_siddiqi@yahoo.com>
2024-04-26 00:30:48 +0500

    Grade page done vechile left bar unresonsive done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Tires from './components/vehicle/Tires'
+import Vehicle from './components/vehicle/Vehicle'
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Tires />} />
+        <Route path="/" element={<Vehicle />} />
      
       </Routes>
+      
+
+      {/* <Vehicle/> */}
     </>
   )
 }
---
+++ src/components/vehicle/Grades.jsx
@@ -0,0 +1,157 @@
+import React from 'react'
+import Car from "../../assets/images/car.png"
+export default function Grades() {
+  return (
+    <>
+      <div className='rounded-xl   bg-white p-5 my-5'>
+        <h2 className='text-3xl text-black font-semibold'>Grades</h2>
+        <div className='flex items-center  justify-center  flex-col xl:flex-row gap-4 my-5'>
+          <div className='w-full xl:w-[25%]'>
+            <img src={Car} alt="car" />
+          </div>
+          <div className='flex flex-col w-full xl:w-[75%]'>
+            <p className='text-black text-xl font-semibold'>Complete Project Car</p>
+            <p className='text-sideText'>2018 2018 Toyota Camry</p>
+            <div className="flex">
+              <svg width="14" height="13" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M7 0L8.65258 4.96591H14L9.674 8.03409L11.326 13L7 9.93012L2.67342 12.9994L4.326 8.03352L0 4.96591H5.34742L7 0Z" fill="#EF730F" />
+              </svg>
+              {/*  */}
+              <svg width="14" height="13" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M7 0L8.65258 4.96591H14L9.674 8.03409L11.326 13L7 9.93012L2.67342 12.9994L4.326 8.03352L0 4.96591H5.34742L7 0Z" fill="#EF730F" />
+              </svg>
+              {/*  */}
+              <svg width="14" height="13" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M7 0L8.65258 4.96591H14L9.674 8.03409L11.326 13L7 9.93012L2.67342 12.9994L4.326 8.03352L0 4.96591H5.34742L7 0Z" fill="#EF730F" />
+              </svg>
+              {/*  */}
+              <svg width="14" height="13" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M7 0L8.65258 4.96591H14L9.674 8.03409L11.326 13L7 9.93012L2.67342 12.9994L4.326 8.03352L0 4.96591H5.34742L7 0Z" fill="#EF730F" />
+              </svg>
+              {/*  */}
+              <svg width="14" height="13" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M7 0L8.65258 4.96591H14L9.674 8.03409L11.326 13L7 9.93012L2.67342 12.9994L4.326 8.03352L0 4.96591H5.34742L7 0Z" fill="#EF730F" />
+              </svg>
+            </div>
+            <p className='text-sideText'>Familarity: I owned this Car</p>
+            <div className='flex' >
+            </div>
+          </div>
+        </div>
+        <hr className='my-3' />
+        <p className='text-sideText'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eget justo luctus urna  see to sollicitudin aliquet. Nunc ut odio et diam maximus luctus. Nunc ut dapibus nisl, id iaculis leo. Sed tincidunt magna arcu, a vestibulum erat eleifend at. Quisque vitae nunc sollicitudin, tristique erat ac, vestibulum to tortor. Etiam ut lorem metus</p>
+        <div className='flex flex-wrap gap-5 my-5 xl:justify-start justify-between'>
+          <div className='w-full  xl:w-fit flex items-center gap-5'>
+            <p className='font-bold text-black'>Frame/Unibody Grade</p>
+            <div className="flex">
+              <svg width="14" height="13" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M7 0L8.65258 4.96591H14L9.674 8.03409L11.326 13L7 9.93012L2.67342 12.9994L4.326 8.03352L0 4.96591H5.34742L7 0Z" fill="#EF730F" />
+              </svg>
+              <svg width="14" height="13" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M7 0L8.65258 4.96591H14L9.674 8.03409L11.326 13L7 9.93012L2.67342 12.9994L4.326 8.03352L0 4.96591H5.34742L7 0Z" fill="#EF730F" />
+              </svg>
+              <svg width="14" height="13" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M7 0L8.65258 4.96591H14L9.674 8.03409L11.326 13L7 9.93012L2.67342 12.9994L4.326 8.03352L0 4.96591H5.34742L7 0Z" fill="#EF730F" />
+              </svg>
+              <svg width="14" height="13" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M7 0L8.65258 4.96591H14L9.674 8.03409L11.326 13L7 9.93012L2.67342 12.9994L4.326 8.03352L0 4.96591H5.34742L7 0Z" fill="#EF730F" />
+              </svg>
+              <svg width="14" height="13" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M7 0L8.65258 4.96591H14L9.674 8.03409L11.326 13L7 9.93012L2.67342 12.9994L4.326 8.03352L0 4.96591H5.34742L7 0Z" fill="#EF730F" />
+              </svg>
+            </div>
+          </div>
+          {/*  */}
+          <div className='w-full  xl:w-fit flex items-center gap-5'>
+            <p className='font-bold text-black'>Frame/Unibody Grade</p>
+            <div className="flex">
+              <svg width="14" height="13" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M7 0L8.65258 4.96591H14L9.674 8.03409L11.326 13L7 9.93012L2.67342 12.9994L4.326 8.03352L0 4.96591H5.34742L7 0Z" fill="#EF730F" />
+              </svg>
+              <svg width="14" height="13" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M7 0L8.65258 4.96591H14L9.674 8.03409L11.326 13L7 9.93012L2.67342 12.9994L4.326 8.03352L0 4.96591H5.34742L7 0Z" fill="#EF730F" />
+              </svg>
+              <svg width="14" height="13" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M7 0L8.65258 4.96591H14L9.674 8.03409L11.326 13L7 9.93012L2.67342 12.9994L4.326 8.03352L0 4.96591H5.34742L7 0Z" fill="#EF730F" />
+              </svg>
+              <svg width="14" height="13" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M7 0L8.65258 4.96591H14L9.674 8.03409L11.326 13L7 9.93012L2.67342 12.9994L4.326 8.03352L0 4.96591H5.34742L7 0Z" fill="#EF730F" />
+              </svg>
+              <svg width="14" height="13" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M7 0L8.65258 4.96591H14L9.674 8.03409L11.326 13L7 9.93012L2.67342 12.9994L4.326 8.03352L0 4.96591H5.34742L7 0Z" fill="#EF730F" />
+              </svg>
+            </div>
+          </div>
+          {/*  */}
+          <div className='w-full  xl:w-fit flex items-center gap-5'>
+            <p className='font-bold text-black'>Tire Grade</p>
+            <div className="flex">
+              <svg width="14" height="13" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M7 0L8.65258 4.96591H14L9.674 8.03409L11.326 13L7 9.93012L2.67342 12.9994L4.326 8.03352L0 4.96591H5.34742L7 0Z" fill="#EF730F" />
+              </svg>
+              <svg width="14" height="13" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M7 0L8.65258 4.96591H14L9.674 8.03409L11.326 13L7 9.93012L2.67342 12.9994L4.326 8.03352L0 4.96591H5.34742L7 0Z" fill="#EF730F" />
+              </svg>
+              <svg width="14" height="13" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M7 0L8.65258 4.96591H14L9.674 8.03409L11.326 13L7 9.93012L2.67342 12.9994L4.326 8.03352L0 4.96591H5.34742L7 0Z" fill="#EF730F" />
+              </svg>
+              <svg width="14" height="13" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M7 0L8.65258 4.96591H14L9.674 8.03409L11.326 13L7 9.93012L2.67342 12.9994L4.326 8.03352L0 4.96591H5.34742L7 0Z" fill="#EF730F" />
+              </svg>
+              <svg width="14" height="13" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M7 0L8.65258 4.96591H14L9.674 8.03409L11.326 13L7 9.93012L2.67342 12.9994L4.326 8.03352L0 4.96591H5.34742L7 0Z" fill="#EF730F" />
+              </svg>
+            </div>
+          </div>
+
+        {/*  */}
+        <div className='w-full  xl:w-fit flex items-center gap-5'>
+            <p className='font-bold text-black'>Mechanical Grade</p>
+            <div className="flex">
+              <svg width="14" height="13" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M7 0L8.65258 4.96591H14L9.674 8.03409L11.326 13L7 9.93012L2.67342 12.9994L4.326 8.03352L0 4.96591H5.34742L7 0Z" fill="#EF730F" />
+              </svg>
+              <svg width="14" height="13" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M7 0L8.65258 4.96591H14L9.674 8.03409L11.326 13L7 9.93012L2.67342 12.9994L4.326 8.03352L0 4.96591H5.34742L7 0Z" fill="#EF730F" />
+              </svg>
+              <svg width="14" height="13" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M7 0L8.65258 4.96591H14L9.674 8.03409L11.326 13L7 9.93012L2.67342 12.9994L4.326 8.03352L0 4.96591H5.34742L7 0Z" fill="#EF730F" />
+              </svg>
+              <svg width="14" height="13" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M7 0L8.65258 4.96591H14L9.674 8.03409L11.326 13L7 9.93012L2.67342 12.9994L4.326 8.03352L0 4.96591H5.34742L7 0Z" fill="#EF730F" />
+              </svg>
+              <svg width="14" height="13" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M7 0L8.65258 4.96591H14L9.674 8.03409L11.326 13L7 9.93012L2.67342 12.9994L4.326 8.03352L0 4.96591H5.34742L7 0Z" fill="#EF730F" />
+              </svg>
+            </div>
+          </div>
+
+        {/*  */}
+        <div className='w-full  xl:w-fit flex items-center gap-5'>
+            <p className='font-bold text-black'>Interior Grade</p>
+            <div className="flex">
+              <svg width="14" height="13" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M7 0L8.65258 4.96591H14L9.674 8.03409L11.326 13L7 9.93012L2.67342 12.9994L4.326 8.03352L0 4.96591H5.34742L7 0Z" fill="#EF730F" />
+              </svg>
+              <svg width="14" height="13" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M7 0L8.65258 4.96591H14L9.674 8.03409L11.326 13L7 9.93012L2.67342 12.9994L4.326 8.03352L0 4.96591H5.34742L7 0Z" fill="#EF730F" />
+              </svg>
+              <svg width="14" height="13" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M7 0L8.65258 4.96591H14L9.674 8.03409L11.326 13L7 9.93012L2.67342 12.9994L4.326 8.03352L0 4.96591H5.34742L7 0Z" fill="#EF730F" />
+              </svg>
+              <svg width="14" height="13" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M7 0L8.65258 4.96591H14L9.674 8.03409L11.326 13L7 9.93012L2.67342 12.9994L4.326 8.03352L0 4.96591H5.34742L7 0Z" fill="#EF730F" />
+              </svg>
+              <svg width="14" height="13" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M7 0L8.65258 4.96591H14L9.674 8.03409L11.326 13L7 9.93012L2.67342 12.9994L4.326 8.03352L0 4.96591H5.34742L7 0Z" fill="#EF730F" />
+              </svg>
+            </div>
+          </div>
+        </div>
+        <button className='bg-primary text-white px-5 py-3 rounded-lg my-4'>
+          Next
+        </button>
+      </div>
+    </>
+  )
+}
