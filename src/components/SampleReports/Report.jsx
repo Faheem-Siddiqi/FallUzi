a25c8ff718308a826a2bb322fdf3a2546d453953
UzairChaudhary <uzairchaudhary.30@gmail.com>
2024-04-27 00:42:36 +0500

    summary section

--- src/components/SampleReports/Report.jsx
+++ src/components/SampleReports/Report.jsx
@@ -4,25 +4,13 @@ import Car from "../../assets/images/car.png"
 import './Report.css'; // Import your CSS file for styling
 function DownloadButton() {
   return (
-    <div className="flex gap-4 px-4 py-4 text-xl font-semibold text-white bg-blue-600 rounded-xl max-md:flex-wrap">
-      <img src="https://cdn.builder.io/api/v1/image/assets/TEMP/28be55babeddb1b2ef295252a56e1cd7d17db1d2d3a770626e0881ace5257463?apiKey=58b353e16f7149db9729650bdfef6357&" alt="Download icon" className="shrink-0 aspect-[1.35] w-[35px]" />
+    <div className="flex gap-2 px-4 py-4 text-base font-semibold text-white bg-blue-600 rounded-xl max-md:flex-wrap">
+      <img src="https://cdn.builder.io/api/v1/image/assets/TEMP/28be55babeddb1b2ef295252a56e1cd7d17db1d2d3a770626e0881ace5257463?apiKey=58b353e16f7149db9729650bdfef6357&" alt="Download icon" className="shrink-0 aspect-[1.35] w-[25px]" />
       <div className="flex-auto">Download Full Size Pictures (PDF)</div>
     </div>
   );
 }
 
-function VehicleSummary() {
-  return (
-    <div className="flex flex-col mt-3.5 text-black max-md:mt-9 max-md:max-w-full">
-      <h1 className="text-4xl font-bold max-md:max-w-full">2018 TOYOTA CAMRY</h1>
-      <h2 className="mt-11 text-3xl font-semibold max-md:mt-10 max-md:max-w-full">SUMMARY:</h2>
-      <p className="mt-8 text-2xl text-black text-opacity-50 max-md:max-w-full">
-        I start this inspection off with the exterior of the vehicle. The front cover has been resprayed, signs of impact on left front of cover, there are chips and scratches flaking off the right side of the cover. The head lights and grill both have chips and the right headlight has a crack in it. The hood has a ding, an outward protrus and touch up paint. The windshield has two chips. The right front fender has chips, and a dent. The right door has a dent, three dings, scratches, and chips. The quarter has a crease in the body near the fuel door, scratches, and a ding above the passenger door window. The rear cover has scratches, chips, touch respray. The trunk and tail lights have scratches. The roof has scratches. The left rear quarter has scratches. The left door has scratches, one ding, chips on mirro scratches along window.         Moving on, I find myself in the interior of the vehicle. I immediately notice signs of cigarette use. There is ash evidence in the ash tray, along the window leading edge, and behind the rear seats once folded down. The driver seat shows signs of wear on the upper bolster. There are scratches along the center console fron select button is missing its top trim. The ash tray doesn't stay open. The cargo nets behind both front seats are torn. The sun visor retainer clips are brok volume knob is not consistent with controlling the volume level. The driver window makes a screeching noise while operating. There are tosee scratches on aa both fron window tint. I scanned the on board computer for trouble codes and foundMoving on, I find myself in the interior of the vehicle. toIimmediately notice signs of cigarette use. There is ash evidence in the ash tray, along the window leading edge, and behind the rear seats once folded down.{" "}
-
-      </p>
-    </div>
-  );
-}
 
 export default function Report() {
   return (
