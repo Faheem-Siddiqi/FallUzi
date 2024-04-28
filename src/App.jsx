90aa66dfaa77acb323d0d9511b24713a9155ea5f
Faheem-Siddiqi <faheem_siddiqi@yahoo.com>
2024-04-28 11:55:44 +0500

    Faheem Pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Tires from './components/vehicle/Tires'
 import Vehicle from './components/vehicle/Vehicle'
+import Report from './components/SampleReports/Report.jsx'
+import VehicleInfo from './components/vehicle/VehicleInfo'
+import Paystubs from './components/vehicle/Paystubs.jsx'
+import Paysubs from './components/Paysubs.jsx'
 function App() {
   return (
     <>
@@ -9,6 +13,9 @@ function App() {
         <Route path="/" element={<Vehicle />} />
         {/* <Sample Report/> */}
         <Route path="/report" element={<Report />} />
+        <Route path="/vehicle" element={<VehicleInfo />} />
+        <Route path="/Paystubs" element={<Paystubs />} />
+        <Route path="/Paysubs" element={<Paysubs />} />
      
       </Routes>
       
